gardener/dashboard

first version in iframe

backend/lib/app.js
@@ -20,7 +20,11 @@ const githubWebhook = require('./github/webhook')
 
 const { healthCheck } = require('./healthz')
 
-const { port, metricsPort } = config
+const {
+  port,
+  metricsPort,
+  luigiEnabled = false
+} = config
 const periodSeconds = config.readinessProbe?.periodSeconds || 10
 
 // resolve pathnames
@@ -39,6 +43,17 @@ if (gitHubRepoUrl) {
   imgSrc.push(url.origin)
   url.hostname = 'media.' + gitHubHostname
   imgSrc.push(url.origin)
+}
+const directives = {
+  defaultSrc: ['\'self\''],
+  connectSrc,
+  styleSrc: ['\'self\'', '\'unsafe-inline\''],
+  fontSrc: ['\'self\'', 'data:'],
+  imgSrc,
+  scriptSrc: ['\'self\'', '\'unsafe-eval\'']
+}
+if (!luigiEnabled) {
+  directives.frameAncestors = ['\'self\'']
 }
 
 // configure app
@@ -64,15 +79,7 @@ app.use('/api', api.router)
 
 app.use(helmet.xssFilter())
 app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ['\'self\''],
-    connectSrc,
-    styleSrc: ['\'self\'', '\'unsafe-inline\''],
-    fontSrc: ['\'self\'', 'data:'],
-    imgSrc,
-    scriptSrc: ['\'self\'', '\'unsafe-eval\''],
-    frameAncestors: ['\'self\'']
-  }
+  directives
 }))
 app.use(helmet.referrerPolicy({
   policy: 'same-origin'
@@ -88,9 +95,11 @@ app.use(expressStaticGzip(PUBLIC_DIRNAME, {
 }))
 app.use(STATIC_PATHS, notFound)
 
-app.use(helmet.frameguard({
-  action: 'deny'
-}))
+if (!luigiEnabled) {
+  app.use(helmet.frameguard({
+    action: 'deny'
+  }))
+}
 app.use(historyFallback(INDEX_FILENAME))
 
 app.use(renderError)

grolu/dashboard_fork

backend/lib/app.js
@@ -30,7 +30,13 @@ import { router as authRouter } from './auth.js'
 import { router as githubWebhookRouter } from './github/webhook/index.js'
 import { healthCheck } from './healthz/index.js'
 
-const { port, host, metricsPort, metricsHost } = config
+const {
+  port,
+  host,
+  metricsPort,
+  metricsHost,
+  luigiEnabled = false,
+} = config
 const periodSeconds = config.readinessProbe?.periodSeconds || 10
 
 // protect against Prototype Pollution vulnerabilities
@@ -66,6 +72,17 @@ if (gitHubRepoUrl) {
   imgSrc.push(url.origin)
   url.hostname = 'media.' + gitHubHostname
   imgSrc.push(url.origin)
+}
+const directives = {
+  defaultSrc: ['\'self\''],
+  connectSrc,
+  styleSrc: ['\'self\'', '\'unsafe-inline\''],
+  fontSrc: ['\'self\'', 'data:'],
+  imgSrc,
+  scriptSrc: ['\'self\'', '\'unsafe-eval\''],
+}
+if (!luigiEnabled) {
+  directives.frameAncestors = ['\'self\'']
 }
 
 const app = express()
@@ -106,15 +123,7 @@ app.use(apiApp)
 
 app.use(helmet.xXssProtection())
 app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ['\'self\''],
-    connectSrc,
-    styleSrc: ['\'self\'', '\'unsafe-inline\''],
-    fontSrc: ['\'self\'', 'data:'],
-    imgSrc,
-    scriptSrc: ['\'self\'', '\'unsafe-eval\''],
-    frameAncestors: ['\'self\''],
-  },
+  directives,
 }))
 app.use(helmet.referrerPolicy({
   policy: 'same-origin',
@@ -153,9 +162,11 @@ app.use(expressStaticGzip(PUBLIC_FS_PATH, {
 
 app.use([BUILD_ASSETS_URL_PATH, STATIC_ASSETS_URL_PATH], notFound)
 
-app.use(helmet.xFrameOptions({
-  action: 'deny',
-}))
+if (!luigiEnabled) {
+  app.use(helmet.xFrameOptions({
+    action: 'deny',
+  }))
+}
 app.use(historyFallback(INDEX_FILENAME))
 
 app.use(renderError)