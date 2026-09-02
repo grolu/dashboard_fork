gardener/dashboard

[openMFP] Move luigiContext and isInIframe to own composables (#2223)
* move luigiContext and isInIframe to own composables

* rename to useOpenMFP

* PR feedback

* PR feedback II

frontend/src/composables/useProjectContext.js
@@ -12,14 +12,13 @@ import {
   provide,
 } from 'vue'
 
-import { useAppStore } from '@/store/app'
 import { useConfigStore } from '@/store/config'
 
 import { cleanup } from '@/composables/helper'
-
-import { useProjectShootCustomFields } from './useProjectShootCustomFields'
-import { useProjectMetadata } from './useProjectMetadata'
-import { useProjectCostObject } from './useProjectCostObject'
+import { useOpenMFP } from '@/composables/useOpenMFP'
+import { useProjectShootCustomFields } from '@/composables/useProjectShootCustomFields'
+import { useProjectMetadata } from '@/composables/useProjectMetadata'
+import { useProjectCostObject } from '@/composables/useProjectCostObject'
 
 import cloneDeep from 'lodash/cloneDeep'
 import get from 'lodash/get'
@@ -28,7 +27,7 @@ import set from 'lodash/set'
 
 export function createProjectContextComposable (options = {}) {
   const {
-    appStore = useAppStore(),
+    openMFP = useOpenMFP(),
     configStore = useConfigStore(),
   } = options
 
@@ -63,9 +62,9 @@ export function createProjectContextComposable (options = {}) {
 
   function createProjectManifest () {
     manifest.value = {}
-    if (appStore.accountId) {
+    if (openMFP.accountId) {
       set(manifest.value, ['metadata', 'label', 'openmfp.org/managed-by'], 'true')
-      set(manifest.value, ['metadata', 'annotations', 'openmfp.org/account-id'], appStore.accountId)
+      set(manifest.value, ['metadata', 'annotations', 'openmfp.org/account-id'], openMFP.accountId)
     }
     initialManifest.value = cloneDeep(normalizedManifest.value)
   }

frontend/src/views/GAdministration.vue
@@ -511,6 +511,7 @@ import GShootCustomFieldsConfiguration from '@/components/GShootCustomFieldsConf
 import GResourceQuotaHelp from '@/components/GResourceQuotaHelp.vue'
 import GTextRouterLink from '@/components/GTextRouterLink.vue'
 
+import { useOpenMFP } from '@/composables/useOpenMFP'
 import { useProvideProjectItem } from '@/composables/useProjectItem'
 import { useProvideProjectContext } from '@/composables/useProjectContext'
 import { useLogger } from '@/composables/useLogger'
@@ -528,6 +529,7 @@ import set from 'lodash/set'
 import includes from 'lodash/includes'
 
 const logger = useLogger()
+const openMFP = useOpenMFP()
 const appStore = useAppStore()
 const configStore = useConfigStore()
 const quotaStore = useQuotaStore()
@@ -540,7 +542,10 @@ const kubeconfigStore = useKubeconfigStore()
 const route = useRoute()
 const router = useRouter()
 
-useProvideProjectContext()
+useProvideProjectContext({
+  openMFP,
+  configStore,
+})
 
 const color = ref('primary')
 const errorMessage = ref(undefined)
@@ -646,9 +651,9 @@ async function updateProperty (key, value, options = {}) {
       metadata: { name },
       spec: { namespace },
     }
-    if (appStore.accountId && !get(projectStore.project, ['metadata', 'annotations', 'openmfp.org/account-id'])) {
+    if (openMFP.accountId && !get(projectStore.project, ['metadata', 'annotations', 'openmfp.org/account-id'])) {
       set(mergePatchDocument, ['metadata', 'labels', 'openmfp.org/managed-by'], 'true')
-      set(mergePatchDocument, ['metadata', 'annotations', 'openmfp.org/account-id'], appStore.accountId)
+      set(mergePatchDocument, ['metadata', 'annotations', 'openmfp.org/account-id'], openMFP.accountId)
     }
     set(mergePatchDocument, ['spec', key], value)
     await projectStore.patchProject(mergePatchDocument)

grolu/dashboard_fork

frontend/src/composables/useProjectContext.js
@@ -13,17 +13,23 @@ import {
 } from 'vue'
 
 import { cleanup } from '@/composables/helper'
-
-import { useProjectShootCustomFields } from './useProjectShootCustomFields'
-import { useProjectMetadata } from './useProjectMetadata'
-import { useProjectCostObject } from './useProjectCostObject'
+import { useOpenMFP } from '@/composables/useOpenMFP'
+import { useProjectShootCustomFields } from '@/composables/useProjectShootCustomFields'
+import { useProjectMetadata } from '@/composables/useProjectMetadata'
+import { useProjectCostObject } from '@/composables/useProjectCostObject'
 
 import cloneDeep from 'lodash/cloneDeep'
 import get from 'lodash/get'
 import isEqual from 'lodash/isEqual'
 import set from 'lodash/set'
 
-export function createProjectContextComposable () {
+export function createProjectContextComposable (options = {}) {
+  const {
+    openMFP = useOpenMFP(),
+  } = options
+
+  const { accountId } = openMFP
+
   function normalizeManifest (value) {
     const object = Object.assign({
       apiVersion: 'core.gardener.cloud/v1beta1',
@@ -55,6 +61,10 @@ export function createProjectContextComposable () {
 
   function createProjectManifest () {
     manifest.value = {}
+    if (accountId.value) {
+      set(manifest.value, ['metadata', 'labels', 'openmfp.org/managed-by'], 'true')
+      set(manifest.value, ['metadata', 'annotations', 'openmfp.org/account-id'], accountId.value)
+    }
     initialManifest.value = cloneDeep(normalizedManifest.value)
   }
 

frontend/src/views/GAdministration.vue
@@ -537,6 +537,7 @@ import GShootCustomFieldsConfiguration from '@/components/GShootCustomFieldsConf
 import GResourceQuotaHelp from '@/components/GResourceQuotaHelp.vue'
 import GTextRouterLink from '@/components/GTextRouterLink.vue'
 
+import { useOpenMFP } from '@/composables/useOpenMFP'
 import { useProvideProjectItem } from '@/composables/useProjectItem'
 import { useProvideProjectContext } from '@/composables/useProjectContext'
 import { useLogger } from '@/composables/useLogger'
@@ -554,6 +555,7 @@ import includes from 'lodash/includes'
 import set from 'lodash/set'
 
 const logger = useLogger()
+const openMFP = useOpenMFP()
 const appStore = useAppStore()
 const configStore = useConfigStore()
 const quotaStore = useQuotaStore()
@@ -566,7 +568,11 @@ const kubeconfigStore = useKubeconfigStore()
 const route = useRoute()
 const router = useRouter()
 
-useProvideProjectContext()
+useProvideProjectContext({
+  openMFP,
+})
+
+const { accountId } = openMFP
 
 const color = ref('primary')
 const errorMessage = ref(undefined)
@@ -672,9 +678,9 @@ async function updateProperty (path, value, options = {}) {
       metadata: { name },
       spec: { namespace },
     }
-    if (appStore.accountId && !projectStore.project.metadata.annotations?.['openmfp.org/account-id']) {
+    if (accountId.value && !projectStore.project.metadata.annotations?.['openmfp.org/account-id']) {
       set(mergePatchDocument, ['metadata', 'labels', 'openmfp.org/managed-by'], 'true')
-      set(mergePatchDocument, ['metadata', 'annotations', 'openmfp.org/account-id'], appStore.accountId)
+      set(mergePatchDocument, ['metadata', 'annotations', 'openmfp.org/account-id'], accountId.value)
     }
     set(mergePatchDocument, path, value)
     await projectStore.patchProject(mergePatchDocument)